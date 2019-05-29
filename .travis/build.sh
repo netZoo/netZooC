54223c7e75f0e35c84f68d3441daea0ed9c3cd32
marouenbg <marouen.b.guebila@gmail.com>
2019-05-29 14:39:25 -0400

    fixed file path in travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pwd
-cd /netzoo-c/panda
+
+cd /panda
 make
 ./PANDA -e ../../testing/ToyExpressionData.txt -m ../../testing/ToyMotifData.txt -p ../../testing/ToyPPIData.txt -a 0.25 -o ToyOutput
