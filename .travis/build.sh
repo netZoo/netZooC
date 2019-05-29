c87af5604141716f8e7c085b4b55527fc73816f1
marouenbg <marouen.b.guebila@gmail.com>
2019-05-29 15:05:10 -0400

    changed test folder permission

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-ls
-pwd
 cd netzoo-c/panda
-pwd
-ls
 make
 ./PANDA -e ../../testing/ToyExpressionData.txt -m ../../testing/ToyMotifData.txt -p ../../testing/ToyPPIData.txt -a 0.25 -o ToyOutput
